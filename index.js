960f35f2c95bf37a18211955fc950ea3fe72f3bb
anthonyolores <tonyoolores@gmail.com>
2019-03-15 14:44:28 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var fs 		= require('fs');
+var app     = express();
+const path = require('path');
 
-const http = require('http');
-const port=process.env.PORT || 3000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+var options = {
+    uri: 'https://www.autoport.nz/api/service.ashx',
+    method: 'POST',
+    json: true,
+    json: {
+      "method": "getvehicles",
+      "content-type": "application.json",
+    }
+  };
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
-
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world')
-})
+	res.send(JSON.stringify({ Hello: ‘World’}));
+});
+
+app.get('/getvehicles', function(req, res) {
+	request(options, function(error, response, body){
+	  if (!error && response.statusCode == 200) {
+		  res.send(body);
+		}
+
+	});
+});
+app.get('/getspecialvehicles', function(req, res) {
+let options2 = options;
+options2.json.method = 'getspecialvehicles';
+request(options2, function(error, response, body){
+  if (!error && response.statusCode == 200) {
+	  res.send(body);
+	}
+
+  });
+});
+app.listen(process.env.PORT || 3000)
